464783c4f61c89ec64cf45445665645ad8d81306
Patryk Markowski <patrol56789@gmail.com>
2024-03-13 15:04:12 +0100

    remove old asset system

--- Dwarf.Engine/AssetSystem/AssetConstants.cs
+++
@@ -1,5 +0,0 @@
-namespace DwarfEngine.AssetSystem;
-public class AssetConstants {
-  public static string AssetNameKeyword = "AssetName";
-  public static string AssetEngineDependeciesKeyword = "AssetEngineDependecies";
-}

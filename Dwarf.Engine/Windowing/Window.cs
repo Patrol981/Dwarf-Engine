864c9e3680edc7c6ca0034a8858fd23f9b5955da
Patryk Markowski <patrol56789@gmail.com>
2025-05-25 02:22:20 +0200

    add macos hint window flag

--- Dwarf.Engine/Windowing/Window.cs
+++ Dwarf.Engine/Windowing/Window.cs
@@ -63,6 +63,8 @@ public class Window : IWindow {
       }
 
       windowFlags |= SDL_WindowFlags.Vulkan;
+    } else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
+      windowFlags |= SDL_WindowFlags.Metal;
     }
 
     if (fullscreen) {
